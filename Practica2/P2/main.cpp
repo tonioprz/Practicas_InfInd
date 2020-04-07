db89b5ef9dc45238ebd37125fce70f446ce46895
Tonio <antoniopgrbb@gmail.com>
2020-04-07 04:35:04 +0200

    P2 hecha a falta de último apartado

--- Practica2/P2/main.cpp
+++ Practica2/P2/main.cpp
@@ -1,8 +1,28 @@
 #include <QCoreApplication>
+#include<iostream>
+#include "person.h"
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    Person p1("Antonio",24); //Uso de constructor parametrizado
+    cout << "Primer apartado: " << endl;
+    cout << "Nombre: " << p1.getName() << endl << "Edad: " << p1.getAge() << endl << endl;
+
+    cout << "Segundo apartado" << endl;
+    Person p2(p1); //Uso de constructor copia
+    cout << "Nombre: " << p1.getName() << endl << "Edad: " << p1.getAge() << endl;
+    cout << "Nombre: " << p2.getName() << endl << "Edad: " << p2.getAge() << endl << endl;
+
+    cout << "Tercer apartado" << endl;
+    Person p3; //Uso de constructor vacío
+    cout << "Nombre: " << p3.getName() << endl << "Edad: " << p3.getAge() << endl;
+    p3.setName("Jose");
+    p3.setAge(30);
+    cout << "Nombre: " << p3.getName() << endl << "Edad: " << p3.getAge() << endl;
 
     return a.exec();
 }
--- Practica2/P2/person.h
+++ Practica2/P2/person.h
@@ -1,11 +1,25 @@
 #ifndef PERSON_H
 #define PERSON_H
-
+#include<iostream>
+using namespace std;
 
 class Person
 {
 public:
-    Person();
+    Person(); //Constructor por defecto
+    Person(string name, int age); // Constructor parametrizado
+    Person(const Person &person); // Constructor copia
+    ~Person(); // Destructor
+    string getName();
+    void setName(string name);
+    int getAge();
+    void setAge(int age);
+
+    Person &operator=(const Person &p);
+
+private:
+    string _name;
+    int _age;
 };
 
 #endif // PERSON_H
